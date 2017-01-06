fb77e654732398aa6c907ca932bc395668f087fc
M <myeveryheart@qq.com>
2017-01-06 14:24:40 +0800

    no message

--- core/src/main/assets/www/cordova/cenariusIntercept.js
+++ core/src/main/assets/www/cordova/cenariusIntercept.js
@@ -1,25 +1,25 @@
-lastXmlhttpRequestPrototypeMethod = null;
-lastXmlhttpRequestPrototypeHeader = null;
-
-XMLHttpRequest.prototype.reallyOpen = XMLHttpRequest.prototype.open;
-XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
-    lastXmlhttpRequestPrototypeMethod = method;
-    this.reallyOpen(method, url, async, user, password);
-};
-
-XMLHttpRequest.prototype.reallySend = XMLHttpRequest.prototype.send;
-XMLHttpRequest.prototype.send = function (body) {
-    window.cenariusInterception.customAjax(lastXmlhttpRequestPrototypeMethod, JSON.stringify(lastXmlhttpRequestPrototypeHeader), body);
-    lastXmlhttpRequestPrototypeMethod = null;
-    lastXmlhttpRequestPrototypeHeader = null;
-    this.reallySend(body);
-};
-
-XMLHttpRequest.prototype.reallySetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
-XMLHttpRequest.prototype.setRequestHeader = function (name, value) {
-    if (!lastXmlhttpRequestPrototypeHeader) {
-        lastXmlhttpRequestPrototypeHeader = {};
-    }
-    lastXmlhttpRequestPrototypeHeader[name] = value;
-    this.reallySetRequestHeader(name, value);
-};
+//lastXmlhttpRequestPrototypeMethod = null;
+//lastXmlhttpRequestPrototypeHeader = null;
+//
+//XMLHttpRequest.prototype.reallyOpen = XMLHttpRequest.prototype.open;
+//XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
+//    lastXmlhttpRequestPrototypeMethod = method;
+//    this.reallyOpen(method, url, async, user, password);
+//};
+//
+//XMLHttpRequest.prototype.reallySend = XMLHttpRequest.prototype.send;
+//XMLHttpRequest.prototype.send = function (body) {
+//    window.cenariusInterception.customAjax(lastXmlhttpRequestPrototypeMethod, JSON.stringify(lastXmlhttpRequestPrototypeHeader), body);
+//    lastXmlhttpRequestPrototypeMethod = null;
+//    lastXmlhttpRequestPrototypeHeader = null;
+//    this.reallySend(body);
+//};
+//
+//XMLHttpRequest.prototype.reallySetRequestHeader = XMLHttpRequest.prototype.setRequestHeader;
+//XMLHttpRequest.prototype.setRequestHeader = function (name, value) {
+//    if (!lastXmlhttpRequestPrototypeHeader) {
+//        lastXmlhttpRequestPrototypeHeader = {};
+//    }
+//    lastXmlhttpRequestPrototypeHeader[name] = value;
+//    this.reallySetRequestHeader(name, value);
+//};
